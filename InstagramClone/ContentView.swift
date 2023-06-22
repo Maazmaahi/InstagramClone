60618b4b749733501ca0d9d597d13e9dadda696b
MaazMaahi <nehalmaaz@gmail.com>
2023-06-22 17:20:55 +0530

    Initial Commit

---
+++ InstagramClone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InstagramClone
+//
+//  Created by Md Maaz Ahmad on 22/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
